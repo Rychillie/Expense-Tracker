db47d3ae4b528c9577d6ba5f63f4222a8caf72ea
Rychillie <14102456+Rychillie@users.noreply.github.com>
2024-05-11 19:36:39 -0300

    Setup

---
+++ Expense Tracker/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  Expense Tracker
+//
+//  Created by Rychillie Umpierre de Oliveira on 11/05/24.
+//
+
+import SwiftUI
+
+enum Category: String {
+    case income = "Income"
+    case expense = "Expense"
+}
---
+++ Expense Tracker/Model/Transaction.swift
@@ -0,0 +1,33 @@
+//
+//  Transaction.swift
+//  Expense Tracker
+//
+//  Created by Rychillie Umpierre de Oliveira on 11/05/24.
+//
+
+import SwiftUI
+
+struct Transaction: Identifiable {
+    let id: UUID = .init()
+    /// Properties
+    var title: String
+    var remarks: String
+    var amount: Double
+    var dateAdded: Date
+    var category: String
+    var tintColor: String
+    
+    init(title: String, remarks: String, amount: Double, dateAdded: Date, category: Category, tintColor: TintColor) {
+        self.title = title
+        self.remarks = remarks
+        self.amount = amount
+        self.dateAdded = dateAdded
+        self.category = category.rawValue
+        self.tintColor = tintColor.color
+    }
+    
+    /// Extracting Color Value from tintColor String
+    var color: Color {
+        return tints.first(where: { $0.color == tintColor })?.value ?? appTint
+    }
+}
