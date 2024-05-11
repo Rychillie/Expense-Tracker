237411cbd4e7c222596015676a015310254a3522
Rychillie <14102456+Rychillie@users.noreply.github.com>
2024-05-11 16:19:01 -0300

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Rychillie Umpierre de Oliveira on 11/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
