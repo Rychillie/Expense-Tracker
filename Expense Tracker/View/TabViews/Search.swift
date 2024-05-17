70a4c628e59c0868e0e586544f59b27bfe6bf6fb
Rychillie <14102456+Rychillie@users.noreply.github.com>
2024-05-17 16:00:09 -0300

    Building Search Page

--- Expense Tracker/View/TabViews/Search.swift
+++ Expense Tracker/View/TabViews/Search.swift
@@ -6,10 +6,38 @@
 //
 
 import SwiftUI
+import Combine
 
 struct Search: View {
+    /// View Properties
+    @State private var searchText: String = ""
+    @State private var filterText: String = ""
+    let searchPublisher = PassthroughSubject<String, Never>()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 15) {
+                    
+                }
+            }
+            .overlay(content: {
+                ContentUnavailableView("Search Transactions", systemImage: "magnifyingglass")
+                    .opacity(searchText.isEmpty ? 1: 0)
+            })
+            .onChange(of: searchText, { oldValue, newValue in
+                if newValue.isEmpty {
+                    filterText = ""
+                }
+                searchPublisher.send(newValue)
+            })
+            .onReceive(searchPublisher.debounce(for: .seconds(0.3), scheduler: DispatchQueue.main), perform: { text in
+                filterText = text
+                print(text)
+            })
+            .searchable(text: $searchText)
+            .navigationTitle("Search")
+            .background(.gray.opacity(0.15))
+        }
     }
 }
 
