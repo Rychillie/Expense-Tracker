20b1c877ffb91f9c535e5573f3aedb732976c361
Rychillie <14102456+Rychillie@users.noreply.github.com>
2024-05-20 21:43:09 -0300

    Implementing Graphs View

---
+++ Expense Tracker/Model/ChartModel.swift
@@ -0,0 +1,22 @@
+//
+//  ChartModel.swift
+//  Expense Tracker
+//
+//  Created by Rychillie Umpierre de Oliveira on 20/05/24.
+//
+
+import SwiftUI
+
+struct ChartGroup: Identifiable {
+    let id: UUID = .init()
+    var date: Date
+    var categories: [ChartCategory]
+    var totalIncome: Double
+    var totalExpense: Double
+}
+
+struct ChartCategory: Identifiable {
+    let id: UUID = .init()
+    var totalValue: Double
+    var category: Category
+}
